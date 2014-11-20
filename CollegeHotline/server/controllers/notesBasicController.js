75eb1aee7319b54858eed9224aac050e583091de
Kenny <kennychow91@yahoo.com>
2014-11-19 20:42:29 -0800

    Updating basic information in notes is functional.

--- CollegeHotline/server/controllers/notesBasicController.js
+++ CollegeHotline/server/controllers/notesBasicController.js
@@ -1,4 +1,4 @@
-var NotesBasic = require('../models/notesBasic');
+var NotesBasic = require('../models/notesBasic.js');
 
 module.exports.list = function(req, res){
 	console.log("notes.list");
@@ -9,15 +9,41 @@ module.exports.list = function(req, res){
 
 module.exports.create = function(req, res){
 	console.log("notes.create");
-	//var newNote = new NotesBasic(req.body);
-	var newNote = new NotesBasic();
-	console.log(newNote);
-	newNote.phoneNumber = "1234567890";
-	newNote.studentName = { first: "Jesus", last: "Christ"};
-	newNote.schoolName = "Jerusalem Independance High"
-	newNote.gpa = "4.0";
-	newNote.graduationYear = "0";
+	/*
+	NotesBasic.update(req.params, {$set: {schoolName : req.schoolName, studentName : {req.studentName, req.studentName}, schoolName : req.schoolName, gpa : req.gpa, graduationYear : req.graduationYear}}, function (err, results){		
+	});
+
 	newNote.save(function (err, result){
 		res.json(result);
 	});
+*/
+	
+	//var newNote = new NotesBasic(req.body);
+	var newNote = new NotesBasic();
+	console.log(req.body);
+		
+	newNote.phoneNumber = req.body.phoneNumber;
+	newNote.studentName = req.body.studentName;
+	newNote.schoolName = req.body.schoolName;
+	newNote.currentYear = req.body.currentYear;
+	
+	newNote.save(function (err, result){
+		res.json(result);
+	});
+}
+
+module.exports.update = function(req, res){
+	NotesBasic.update(
+			{phoneNumber : req.body.phoneNumber},
+			{$set: 
+					{studentName : req.body.studentName, 
+					schoolName : req.body.schoolName, 
+					currentYear : req.body.currentYear
+					} 
+			},
+			function (err, results)
+			{
+				res.json(results);
+			}
+	);
 }
