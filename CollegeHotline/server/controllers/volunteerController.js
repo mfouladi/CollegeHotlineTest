084658a69f5ba1d59dba682b9f7d3dceb168c152
mfouladi <fouladimatin@gmail.com>
2014-12-11 03:31:06 -0800

    fixed volunteer html and sorted by availability

--- CollegeHotline/server/controllers/volunteerController.js
+++ CollegeHotline/server/controllers/volunteerController.js
@@ -1,7 +1,7 @@
 var Volunteer 	= require('../models/volunteer.js')
 
 module.exports.listVolunteers = function(req, res){
-	Volunteer.find(req.params, function (err, volunteer){
+	Volunteer.find(req.params).sort({ available: 'desc'}).exec(function (err, volunteer){
 		res.json(volunteer);
 	});
 }
