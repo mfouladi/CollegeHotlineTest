19f60c001ae902bb637f1b2b64189297c6d36e47
mfouladi <fouladimatin@gmail.com>
2015-11-15 20:17:27 -0800

    Volunteers Page - Added yellow background for volunteer that is on a call

--- CollegeHotline/client/js/controllers/volunteerController.js
+++ CollegeHotline/client/js/controllers/volunteerController.js
@@ -5,10 +5,33 @@ app.controller('volunteerController',['$scope', '$resource',
 
 	$scope.volunteers = [];
 
+	function sortVolunteers(a, b){
+		if(a.available && !b.available){
+			return -1;
+		} else if(!a.available && b.available){
+			return 1;
+		} else if(a.available && b.available){
+			return 0;
+		} else if(a.online && !b.online){
+			return -1;
+		} else if(!a.online && b.online){
+			return 1;
+		} else if(a.online && b.online){
+			return 0;
+		} else if(a.firstName < b.firstName){
+			return -1;
+		} else if(a.firstName > b.firstName){
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
 	//Query active and inactive conversations
 	function updatePage(){
 		//Find Inactive conversations
 		VolunteerStatus.query({}, function (results){
+			results.sort(sortVolunteers);
 			$scope.volunteers = results;
 		});
 
