8e809725a844a4e79949941c8e36691dffff1549
mfouladi <fouladimatin@gmail.com>
2016-02-14 00:08:13 -0800

    Format and lint client/loginController.js

--- CollegeHotline/client/js/controllers/loginController.js
+++ CollegeHotline/client/js/controllers/loginController.js
@@ -1,16 +1,20 @@
-app.controller('loginController',['$scope', '$resource', function ($scope, $resource) {
+/*jshint strict: true, unused: false */
+/* globals app, $scope */
+"use strict";
+
+app.controller('loginController', ['$scope', '$resource', function ($scope, $resource) {
 
 	$scope.isLoggedIn = false;
 
 	var Login = $resource('/loggedin');
 
-	Login.query({}, function (results){
-		if(results.length > 0)
+	Login.query({}, function (results) {
+		if (results.length > 0)
 			$scope.isLoggedIn = true;
 		else
 			$scope.isLoggedIn = false;
 	});
 
-	
+
 
 }]);
