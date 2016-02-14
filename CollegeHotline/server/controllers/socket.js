5321c6e10ed0fb18835e6134a830d06b26cb4b69
mfouladi <fouladimatin@gmail.com>
2016-02-13 23:44:40 -0800

    Format and lint socket.js

--- CollegeHotline/server/controllers/socket.js
+++ CollegeHotline/server/controllers/socket.js
@@ -1,10 +1,13 @@
+/*jshint strict: true, node: true, unused: false */
+"use strict";
+
 /*
  * Serve content over a socket
  */
 
-var Volunteer 	= require('../models/volunteer.js')
-var Conversation 	= require('../models/conversation.js')
+var Volunteer = require('../models/volunteer.js');
+var Conversation = require('../models/conversation.js');
 
-module.exports = function (socket, req, res ) {
+module.exports = function (socket, req, res) {
 
 };
