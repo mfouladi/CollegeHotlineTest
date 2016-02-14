0398e1a5443e0c89b701e37e68491e1ba11e6f2b
mfouladi <fouladimatin@gmail.com>
2016-02-14 00:14:41 -0800

    Format and lint client/profileController.js

--- CollegeHotline/client/js/controllers/profileController.js
+++ CollegeHotline/client/js/controllers/profileController.js
@@ -1,6 +1,11 @@
-app.controller('profileController',['$scope', '$resource', 'availibilityTimer', 
+/*jshint strict: true, unused: false */
+/* globals angular, app, $scope */
+"use strict";
+
+app.controller('profileController', ['$scope', '$resource', 'availibilityTimer',
 	function ($scope, $resource, availibilityTimer) {
 
-	availibilityTimer.timer();
-	
-}]);
+		availibilityTimer.timer();
+
+	}
+]);
