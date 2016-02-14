4e59eb53bf2bd7cf1dd676a52c68cc1a44d58d16
mfouladi <fouladimatin@gmail.com>
2016-02-14 00:16:13 -0800

    Format and lint client/signupController.js

--- CollegeHotline/client/js/controllers/signupController.js
+++ CollegeHotline/client/js/controllers/signupController.js
@@ -1,33 +1,35 @@
-app.controller('signupController',['$scope', '$resource', function ($scope, $resource) {
+/*jshint strict: true, unused: false */
+/* globals angular, app, $scope */
+"use strict";
+
+app.controller('signupController', ['$scope', '$resource', function ($scope, $resource) {
 	$scope.passwordMatch = false;
 	$scope.emptyRequiredField = false;
 	$scope.signupError = true;
 
 
-	$scope.checkPassword = function(){
-		if($scope.username == '' || $scope.username === undefined ||
-		   $scope.phoneNumber == '' || $scope.phoneNumber === undefined ||
-		   $scope.firstName == '' || $scope.firstName === undefined ||
-		   $scope.lastName == '' || $scope.lastName === undefined ||
-		   $scope.email == '' || $scope.email === undefined ||
-		   $scope.phoneNumber == '' || $scope.phoneNumber === undefined){
+	$scope.checkPassword = function () {
+		if ($scope.username === '' || $scope.username === undefined ||
+			$scope.phoneNumber === '' || $scope.phoneNumber === undefined ||
+			$scope.firstName === '' || $scope.firstName === undefined ||
+			$scope.lastName === '' || $scope.lastName === undefined ||
+			$scope.email === '' || $scope.email === undefined ||
+			$scope.phoneNumber === '' || $scope.phoneNumber === undefined) {
 			$scope.emptyRequiredField = true;
-		}
-		else{
+		} else {
 			$scope.emptyRequiredField = false;
 		}
 
-		if($scope.password !== undefined && $scope.password != ''){
-			if($scope.password != $scope.confirmPassword){
+		if ($scope.password !== undefined && $scope.password !== '') {
+			if ($scope.password !== $scope.confirmPassword) {
 				$scope.passwordMatch = true;
-			}
-			else{
+			} else {
 				$scope.passwordMatch = false;
 			}
 		}
 
 		$scope.signupError = $scope.emptyRequiredField && $scope.passwordMatch;
-	}
-	
+	};
+
 
 }]);
