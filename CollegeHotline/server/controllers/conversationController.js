49033c36703f42962039b78abc2c720837130d06
mfouladi <fouladimatin@gmail.com>
2016-02-13 23:37:42 -0800

    Format and lint conversationController.js

--- CollegeHotline/server/controllers/conversationController.js
+++ CollegeHotline/server/controllers/conversationController.js
@@ -1,127 +1,165 @@
-var Conversation 	= require('../models/conversation.js');
-var express    = require('express');
-var router 	   = express.Router();
+/*jshint strict: true, node: true, unused: false */
+"use strict";
+
+var Conversation = require('../models/conversation.js');
+var express = require('express');
+var router = express.Router();
 
 
-function createConversation(req, res){
-	if (req.body.From){
+function createConversation(req, res) {
+	if (req.body.From) {
 		req.body.phoneNumber = req.body.From;
 	}
-	if (req.body.Text){
-		req.body.text = req.body.Text;		
+	if (req.body.Text) {
+		req.body.text = req.body.Text;
 	}
 
-	d = new Date();
-	year = d.getFullYear();
-	month = d.getMonth();
-	day = d.getDate();
-	hour = d.getHours();
-	minute = d.getMinutes();
-	
-	if(hour < 12)
-		label = "AM"
-	else
-		label = "PM"
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth();
+	var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var label = "AM";
 
-	if(hour > 12)
+	if (hour > 12) {
 		hour = hour % 12;
-
-	if(day < 10)
-		day = "0"+day;
-
-	if(minute < 10)
-		minute = "0"+minute;
-
-	if(hour == 0)
+		label = "PM";
+	} else if (hour === 0) {
 		hour = 12;
+	}
 
-	stamp = month+"/"+day+"/"+year+" at "+hour+":"+minute+label;
-	Conversation.find({phoneNumber: req.body.phoneNumber}, function (err, result){
-		if(result.length == 0){
+	if (day < 10) {
+		day = "0" + day;
+	}
+
+	if (minute < 10) {
+		minute = "0" + minute;
+	}
+
+	var timestamp = month + "/" + day + "/" + year + " at " + hour + ":" + minute + label;
+	Conversation.find({
+		phoneNumber: req.body.phoneNumber
+	}, function (err, result) {
+		if (result.length === 0) {
 
 			var conversation = new Conversation();
 			var newMessage = {
-								text			: req.body.text,
-								timeStamp		: Date(),
-								timeStampString : stamp,
-								isVolunteer		: false,
-								volunteerID		: "none",
-							 };
+				text: req.body.text,
+				timeStamp: Date(),
+				timeStampString: timestamp,
+				isVolunteer: false,
+				volunteerID: "none",
+			};
 			conversation.messages.push(newMessage);
 			conversation.phoneNumber = req.body.phoneNumber;
-			conversation.save(function (err, result){
+			conversation.save(function (err, result) {
 				res.json(result);
 			});
-		}
-
-		else{
-			if(!req.body.isVolunteer){
-				Conversation.update({phoneNumber: req.body.phoneNumber}, 
-									{$push: {"messages": 
-												{
-													text			: req.body.text,
-													timeStamp		: Date(),
-													timeStampString : stamp,
-													isVolunteer		: false,
-													volunteerID		: "none",
-												}
-											},
-									$inc: {messageCount : 1, unreadMessageCount : 1, unansweredMessageCount : 1}
-									}, 
-									function (err, result){
-										res.json(result);
-									});
-			}
-			else{
-				Conversation.update({phoneNumber: req.body.phoneNumber}, 
-									{$push: {"messages": 
-												{
-													text			: req.body.text,
-													timeStamp		: Date(),
-													timeStampString : stamp,
-													isVolunteer		: true,
-													volunteerID		: req.user[0].id,
-													volunteerName	: req.user[0].username
-												}
-											},
-									$inc: {messageCount : 1},
-									$set: {unansweredMessageCount: 0, unreadMessageCount: 0}
-									}, 
-									function (err, result){
-										res.json(result);
-									});
+		} else {
+			if (!req.body.isVolunteer) {
+				Conversation.update({
+						phoneNumber: req.body.phoneNumber
+					}, {
+						$push: {
+							"messages": {
+								text: req.body.text,
+								timeStamp: Date(),
+								timeStampString: timestamp,
+								isVolunteer: false,
+								volunteerID: "none",
+							}
+						},
+						$inc: {
+							messageCount: 1,
+							unreadMessageCount: 1,
+							unansweredMessageCount: 1
+						}
+					},
+					function (err, result) {
+						res.json(result);
+					});
+			} else {
+				Conversation.update({
+						phoneNumber: req.body.phoneNumber
+					}, {
+						$push: {
+							"messages": {
+								text: req.body.text,
+								timeStamp: Date(),
+								timeStampString: timestamp,
+								isVolunteer: true,
+								volunteerID: req.user[0].id,
+								volunteerName: req.user[0].username
+							}
+						},
+						$inc: {
+							messageCount: 1
+						},
+						$set: {
+							unansweredMessageCount: 0,
+							unreadMessageCount: 0
+						}
+					},
+					function (err, result) {
+						res.json(result);
+					});
 			}
 		}
 	});
 }
 
-function activateConversation(req, res){
-	Conversation.findOneAndUpdate(req.params, {$set: {active : true, currentVolunteerID: req.user[0].id}}, function (err, conversation){
+function activateConversation(req, res) {
+	Conversation.findOneAndUpdate(req.params, {
+		$set: {
+			active: true,
+			currentVolunteerID: req.user[0].id
+		}
+	}, function (err, conversation) {
 		res.json(conversation);
 	});
 }
 
-function deactivateConversation(req, res){
-	Conversation.findOneAndUpdate(req.params, {$set: {active : false, currentVolunteerID: "none"}}, function (err, conversation){
+function deactivateConversation(req, res) {
+	Conversation.findOneAndUpdate(req.params, {
+		$set: {
+			active: false,
+			currentVolunteerID: "none"
+		}
+	}, function (err, conversation) {
 		res.json(conversation);
 	});
 }
 
-function listInactiveConversations(req, res){
-	Conversation.find({$and: [{active: false}, {unansweredMessageCount : {$gt: 0}}]}, function (err, results){
+function listInactiveConversations(req, res) {
+	Conversation.find({
+		$and: [{
+			active: false
+		}, {
+			unansweredMessageCount: {
+				$gt: 0
+			}
+		}]
+	}, function (err, results) {
 		res.json(results);
 	});
 }
 
-function listActiveConversations(req, res){
-	Conversation.find({currentVolunteerID: req.user[0].id}, function (err, results){
+function listActiveConversations(req, res) {
+	Conversation.find({
+		currentVolunteerID: req.user[0].id
+	}, function (err, results) {
 		res.json(results);
 	});
 }
 
 
-function openConversation (req, res){
-	Conversation.findOneAndUpdate(req.params, {$set: {unreadMessageCount : 0}}, function (err, conversation){
+function openConversation(req, res) {
+	Conversation.findOneAndUpdate(req.params, {
+		$set: {
+			unreadMessageCount: 0
+		}
+	}, function (err, conversation) {
 		res.json(conversation);
 	});
 }
